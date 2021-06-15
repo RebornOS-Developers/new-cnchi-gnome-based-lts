7cf0ed94569353f8cf6c47cabaf96fdb54b2ecd0
Rafael <85809040+rafaeluy@users.noreply.github.com>
2021-06-15 10:22:58 -0300

    Update cnchi-start.sh

--- airootfs/usr/bin/cnchi-start.sh
+++ airootfs/usr/bin/cnchi-start.sh
@@ -8,4 +8,5 @@ sudo chmod 644 /etc/pacman.d/mirrorlist
 sudo rm /home/rebornos/Downloads/reborn-mirrorlist
 sudo rm /home/rebornos/Downloads/mirrorlist
 sudo chmod 644 /etc/pacman.conf
+sudo chmod 0777 /usr/share/cnchi/scripts/ckbcomp
 sudo -E /usr/bin/python -Wall /usr/share/cnchi/src/cnchi.py -dvz --no-check --packagelist /usr/share/cnchi/data/packages.xml
