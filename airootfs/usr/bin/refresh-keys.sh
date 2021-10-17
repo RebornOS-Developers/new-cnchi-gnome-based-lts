240ce0076acf18f784a7ce29957839cd14ecfffc
Rafael <85809040+rafaeluy@users.noreply.github.com>
2021-10-17 16:06:00 -0300

    Change rebornos-iso-welcome to rebornos-iso

--- airootfs/usr/bin/refresh-keys.sh
+++ airootfs/usr/bin/refresh-keys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.gedit.desktop', 'gparted.desktop', 'rebornos-install.desktop', 'rebornos-install-2.desktop', 'rebornos-iso-welcome.desktop', 'org.gnome.Terminal.desktop']"
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.gedit.desktop', 'gparted.desktop', 'rebornos-install.desktop', 'rebornos-install-2.desktop', 'rebornos-welcome.desktop', 'org.gnome.Terminal.desktop']"
 sudo systemctl enable --now NetworkManager.service
 sudo systemctl enable --now bluetooth.service
 sudo rm -rf /etc/pacman.d/gnupg
