0987fcd8e161f493ee7d39254fa4ca61cfa28a90
Rafael Costa Rega <rcostarega@gmail.com>
2021-10-20 11:51:04 -0300

    Bug fix...

--- airootfs/usr/bin/cnchi-start-2.sh
+++ airootfs/usr/bin/cnchi-start-2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo pacman -S new-cnchi --noconfirm
+sudo pacman -S new-cnchi-lts --noconfirm
 sudo chmod 0777 /usr/share/cnchi/scripts/ckbcomp
 sudo -E /usr/bin/python -Wall /usr/share/cnchi/src/cnchi.py -dvz --no-check --packagelist /usr/share/cnchi/data/packages.xml
